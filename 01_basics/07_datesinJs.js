c8db9ad489fb17b5d41943fa3fd408d2aed46150
AnujJain1011 <112792339+AnujJain1011@users.noreply.github.com>
2024-03-16 16:03:11 +0000

    date in js

--- 01_basics/07_datesinJs.js
+++ 01_basics/07_datesinJs.js
@@ -1,2 +1,9 @@
-hjhjhhjjkkbjbjbjnbnbnbmmnm
-hjgjgjgjgjgh
+let myDate = new Date();
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toISOString());
+
+// console.log(typeof myDate); // Date is a object in js
+// let myCreatedDate = new Date(2024,2,16);
+let myCreatedDate = new Date("03-16-2023");
+console.log(myCreatedDate.toLocaleDateString());
