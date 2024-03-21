44a44d728d9f6e3c4e3d8718daed60946dfe6a3e
AnujJain1011 <112792339+AnujJain1011@users.noreply.github.com>
2024-03-21 03:38:42 +0000

    changed

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -1,10 +1,20 @@
 const name = "hitesh"
 const repoCount = 50
 
-console.log(name + repoCount+ " Value");
-console.log(`My name is ${name} and my repoCount is ${repoCount}`); // string interpolation
+// console.log(name + repoCount+ " Value");
+// console.log(`My name is ${name} and my repoCount is ${repoCount}`); // string interpolation
 
 const gameName = new String("hitesh-hc");
-console.log(gameName);
+// console.log(gameName.__proto__);//prototype of the object is it's property which is itself an object
 
-trim slice substring replace 
+// console.log(typeof gameName);//object hai
+
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('t'));
+
+console.log(gameName);
+console.log(gameName.substr(-5,3));//not ecmascript official,treats -5 as reverse direction
+console.log(gameName.substring(-5,3)); //brings -ve to 0,swaps values accordingly
+
+
+// trim slice substring replace 
