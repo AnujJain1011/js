6ee3d5c721fd969a1a03985e5e162b7a714272b6
AnujJain1011 <112792339+AnujJain1011@users.noreply.github.com>
2024-03-21 16:44:15 +0000

    changes

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -4,7 +4,7 @@ const repoCount = 50
 // console.log(name + repoCount+ " Value");
 // console.log(`My name is ${name} and my repoCount is ${repoCount}`); // string interpolation
 
-const gameName = new String("hitesh-hc");
+let gameName = new String("hitesh-hc");
 // console.log(gameName.__proto__);//prototype of the object is it's property which is itself an object
 
 // console.log(typeof gameName);//object hai
@@ -13,8 +13,17 @@ const gameName = new String("hitesh-hc");
 // console.log(gameName.indexOf('t'));
 
 console.log(gameName);
-console.log(gameName.substr(-5,3));//not ecmascript official,treats -5 as reverse direction
-console.log(gameName.substring(-5,3)); //brings -ve to 0,swaps values accordingly
+// console.log(gameName.substr(-5,3));//not ecmascript official,treats -5 as reverse direction
+// console.log(gameName.substring(-5,3)); //brings -ve to 0,swaps values accordingly
 
+// const anotherString = gameName.slice(-8,10);
+// console.log(anotherString);
 
+// const anotherString = "  hello hitesh. ";
+// console.log(anotherString);
+// console.log(anotherString.trim());
+
+gameName = "hitesh-hc-com";
+console.log(gameName);
+console.log(gameName.split(""));
 // trim slice substring replace 
--- 01_basics/07_datesinJs.js
+++ 01_basics/07_datesinJs.js
@@ -5,5 +5,16 @@ let myDate = new Date();
 
 // console.log(typeof myDate); // Date is a object in js
 // let myCreatedDate = new Date(2024,2,16);
-let myCreatedDate = new Date("03-16-2023");
-console.log(myCreatedDate.toLocaleDateString());
+
+// let myCreatedDate = new Date("03-16-2023");// MM-DD-YYYY
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now();
+// console.log(myTimeStamp);
+// let myAnotherDate = new Date(2022,1,5,3,33,40);
+// console.log(myAnotherDate.toLocaleString());
+
+console.log(Date.now());//current date in milliseconds
+console.log(Math.floor(Date.now()/1000));//current date in seconds
+
+// toLocaleString is used to customize date formats
