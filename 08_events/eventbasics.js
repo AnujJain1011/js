b189e4684f70ed50a83a1cdf2b08a4f98b0e8fc1
AnujJain1011 <112792339+AnujJain1011@users.noreply.github.com>
2024-04-04 10:37:33 +0000

    events

--- 08_events/eventbasics.js
+++ 08_events/eventbasics.js
@@ -1,1 +1,5 @@
 console.log("Getting started with event");
+// 1.Javascript is sequentially run language means all the events in javascript run sequentially,but some events like browser events run only when invoked by an user activity in ashychronous manner
+
+// 2.some properties and methods of Events 
+// type,timestamp,defaultprevented,target,toElement,srcElement,currentElement,clientX,clientY,screenX,screenY,altKey,ctrlKey,shiftKey,keyCode
