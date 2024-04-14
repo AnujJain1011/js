b8f3901bb972be5ffdc57be607c30f606ea5e0d6
AnujJain1011 <112792339+AnujJain1011@users.noreply.github.com>
2024-04-14 10:37:27 +0000

    changes

--- 10_classes_and_oop/call.js
+++ 10_classes_and_oop/call.js
@@ -6,9 +6,10 @@ function setUserName(username){
 
 //if we use setUserName() only then all the information of setUserName() will be lost because of removal of it from call stack
 function createUser(username,email,password){ 
-    setUserName.call(this,username)  // whenever call() is used first arguement(this) is optional that refers to the current context,call() stores all the details of the function accompanied
+    // setUserName.call(this,username)  // whenever call() is used first arguement(this) is optional that refers to the current context,call() stores all the details of the function accompanied
     this.email = email
     this.password = password
+    // setUserName(username)
 }
 
 const chai = new createUser('chaiaurcode','chai@fb.com',1243);
