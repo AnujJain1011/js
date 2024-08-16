b751d3c878f083657b33bd3171162f9cdfed1ec2
AnujJain1011 <112792339+AnujJain1011@users.noreply.github.com>
2024-08-16 13:17:25 +0530

    creation of objects,symbols
    //two ways to create objects using literals and constructor
    //if object is created using constructors then it is singleton,means only one copy of that object is available but in case 
    of literals multiple instance of objects can be their
    
    // const mySymbol = Symbol(optional string);// symbol declaration
    //the only way to add symbol to the object is by placing symbol into [symbol_name] : "";

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -1,5 +1,6 @@
-//literal , constructor
-//if object is created using constructors then it is singleton,means only one copy of that object is available but in case of literals multiple instance of objects can be their
+//two ways to create objects using literal and constructor
+//if object is created using constructors then it is singleton,means only one copy of that object is available but in case 
+of literals multiple instance of objects can be their
 
 const mySym = Symbol("key1");//symbol : used for unique values
 const mySym2 = Symbol("key2");
@@ -9,11 +10,15 @@ const jsUser = {
     age:21,
     [mySym]:"myKey1"
 }
+
+// const mySymbol = Symbol(optional string);
+//the only way to add symbol to the object is placing symbol into [symbol_name] 
+
 console.log(jsUser); //dot notation
-console.log(jsUser["full name"]); //square notation
+console.log(jsUser["name"]); //square notation  ,here all the keys behave like string inside js
 // console.log(typeof jsUser.mySym);
 
-Object.freeze(jsUser);//freezez the object ,value can not be changed
+Object.freeze(jsUser);//freezez the object ,It will not show any error but value can not be changed after this. 
 
 jsUser.age = 22;
 // console.log(jsUser);
